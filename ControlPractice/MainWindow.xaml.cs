335305c8caa7f2989439248fb76936b365ed067c
sosuisen <hidekazu.kubota@gmail.com>
2023-12-30 17:58:56 +0900

    fix: add project

---
+++ ControlPractice/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ControlPractice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var tb = this.FindName("MyTextBox") as TextBox;
+            var label = this.FindName("MyLabel") as Label;
+            label!.Content = $"Hello,{tb!.Text}!";
+        }
+    }
+}
